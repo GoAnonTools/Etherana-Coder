02613bbe2481ed1bb300c39044d912c1d9b08167
GoAnonTools <etherana.com@gmail.com>
2026-06-16 21:27:00 +0200

    Neutralize 1DS telemetry transport

--- src/vs/platform/telemetry/browser/1dsAppender.ts
+++ src/vs/platform/telemetry/browser/1dsAppender.ts
@@ -5,20 +5,17 @@
 
 import { AbstractOneDataSystemAppender, IAppInsightsCore } from '../common/1dsAppender.js';
 
-
 export class OneDataSystemWebAppender extends AbstractOneDataSystemAppender {
+
 	constructor(
 		isInternalTelemetry: boolean,
 		eventPrefix: string,
 		defaultData: { [key: string]: any } | null,
-		iKeyOrClientFactory: string | (() => IAppInsightsCore), // allow factory function for testing
+		iKeyOrClientFactory: string | (() => IAppInsightsCore),
 	) {
 		super(isInternalTelemetry, eventPrefix, defaultData, iKeyOrClientFactory);
 
-		// If we cannot fetch the endpoint it means it is down and we should not send any telemetry.
-		// This is most likely due to ad blockers
-		fetch(this.endPointHealthUrl, { method: 'GET' }).catch(err => {
-			this._aiCoreOrKey = undefined;
-		});
+		// Etherana Coder is privacy-first: do not ping Microsoft 1DS health endpoints.
+		this._aiCoreOrKey = undefined;
 	}
 }
